80962c411f042577b3ed7c7ea171378aad0b33ff
raHul-stc <rrahul772k@gmail.com>
2026-07-07 23:28:45 +0530

    Fix static files on Vercel

--- Resume/settings.py
+++ Resume/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
 
 
